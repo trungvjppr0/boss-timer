5bfd1bb4adac0b8c9ae80556a58ae09df6c1fc6c
Vjppr0 <trunghqhe160184@fpt.edu.vn>
2026-06-11 16:25:20 +0700

    Update timer-worker.js

--- timer-worker.js
+++ timer-worker.js
@@ -1,1 +1,24 @@
+let interval = null;
 
+self.onmessage = function (e) {
+
+  if (e.data === "start") {
+
+    if (interval) clearInterval(interval);
+
+    interval = setInterval(() => {
+
+      self.postMessage({
+        type: "tick",
+        time: Date.now()
+      });
+
+    }, 10000);
+
+  }
+
+  if (e.data === "stop") {
+    clearInterval(interval);
+    interval = null;
+  }
+};
